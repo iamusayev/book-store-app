3b7600b33b754193986fb6d34cd96a599853e147
iamusayev <iamusayev1337@gmail.com>
2022-04-28 10:43:11 +0400

    Added mappers

---
+++ src/main/java/com/example/bookstoreapp/mapper/PublisherMapper.java
@@ -0,0 +1,22 @@
+package com.example.bookstoreapp.mapper;
+
+
+
+import com.example.bookstoreapp.dao.entity.PublisherEntity;
+import model.dto.PublisherAndBooksListDto;
+
+import java.util.stream.Collectors;
+
+
+public class PublisherMapper {
+
+
+    public static PublisherAndBooksListDto mapEntityToPublisherBooksDto(PublisherEntity entity){
+        return PublisherAndBooksListDto.builder()
+                .name(entity.getName())
+                .bookListDto(entity.getBooks().stream()
+                        .map(BookMapper::mapEntityToDto)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
---
+++ src/main/java/com/example/bookstoreapp/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.example.bookstoreapp.mapper;
+
+
+import com.example.bookstoreapp.dao.entity.UserEntity;
+import model.dto.UserRegisterDto;
+
+public  class UserMapper {
+
+    public static UserEntity mapRegisterDtoToEntity(UserRegisterDto dto){
+        return UserEntity.builder()
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .age(dto.getAge())
+                .username(dto.getUsername())
+                .password(dto.getPassword())
+                .email(dto.getEmail())
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .credentialsNonExpired(true)
+                .enabled(true)
+                .build();
+    }
+}
