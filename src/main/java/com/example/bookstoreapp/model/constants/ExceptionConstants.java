373272694591a8153a5abe685cea00d8e6fc2d0f
Anar <95879636+iamusayev@users.noreply.github.com>
2022-04-30 21:09:12 +0400

    Addede exception constants

--- src/main/java/com/example/bookstoreapp/model/constants/ExceptionConstants.java
+++ src/main/java/com/example/bookstoreapp/model/constants/ExceptionConstants.java
@@ -2,8 +2,10 @@ package com.example.bookstoreapp.model.constants;
 
 public class ExceptionConstants {
 
-    public static final String VALIDATION_EXCEPTION_CODE ="INVALID_MANDATORY_FIELD";
+   public static final String VALIDATION_EXCEPTION_CODE ="INVALID_MANDATORY_FIELD";
     public static final String VALIDATION_EXCEPTION_MESSAGE = "One or more required fields in the request are invalid";
+
+    public static final String VALIDATION_MESSAGE = "This %s %s %s already exist";
 
     public static final String UNEXPECTED_EXCEPTION_CODE = "UNEXPECTED_EXCEPTION";
     public static final String UNEXPECTED_EXCEPTION_MESSAGE = "Unexpected exception occurred";
@@ -14,8 +16,4 @@ public class ExceptionConstants {
 
     public static final String BOOK_UPDATE_CODE = "UPDATE_BOOK_EXCEPTION";
     public static final String BOOK_UPDATE_MESSAGE = "You can't update this because you are not the publisher of this book";
-
-
-    public static final String SQL_VALIDATION_CODE = "SQL_VALIDATION_EXCEPTION";
-    public static final String SQL_VALIDATION_MESSAGE = "You violated the uniqueness of some value";
 }
